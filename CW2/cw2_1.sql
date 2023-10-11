0c6d9b5ad4c8f8445904510e739921398c43dbdf
luin1 <adas.lui@gmail.com>
2023-10-11 11:01:17 +0200

    Create cw2_1.sql

---
+++ CW2/cw2_1.sql
@@ -0,0 +1,7 @@
+drop table "WASILEWSKIA"."REGIONS" cascade constraints PURGE;
+drop table "WASILEWSKIA"."COUNTRIES" cascade constraints PURGE;
+drop table "WASILEWSKIA"."LOCATIONS" cascade constraints PURGE;
+drop table "WASILEWSKIA"."JOBS" cascade constraints PURGE;
+drop table "WASILEWSKIA"."JOB_HISTORY" cascade constraints PURGE;
+drop table "WASILEWSKIA"."DEPARTAMENTS" cascade constraints PURGE;
+drop table "WASILEWSKIA"."EMPLOYEES" cascade constraints PURGE;
